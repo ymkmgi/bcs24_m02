326a06c032781acba3f6644df9ea7e3244a7ff3c
Ayumi Kumagai <yumimori22@gmail.com>
2023-02-21 14:54:12 +0800

    include javascript info

---
+++ index.js
@@ -0,0 +1,16 @@
+// ctrl + / -> sing comment (//)
+// ctrl + shift + / -> multiple comments (/** */)
+
+/**[SECTION] Statements and Comments
+ * statements
+ * - Statements in programming are instructions that we tell the computer to perform
+ * - JS statements usually end with semi-colon
+ * 
+ * syntax
+ * - in  prgramming, it is the set of rules that describe how the statements must be constructed
+ * 
+ * comments
+ * - are parts of the code that gets ignored by the language
+ * - are meant to describe the written code
+ * 
+ */
