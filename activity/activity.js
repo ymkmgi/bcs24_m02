83b11e2ca2ad859b10d29f6f804a066b9a37db12
Ayumi Kumagai <yumimori22@gmail.com>
2023-02-28 17:52:55 +0800

    discussion 2/28/23

---
+++ activity/activity.js
@@ -0,0 +1,17 @@
+let food = "Carbonara";
+console.log(`Favorite food: ${food}`);
+
+const sum = 150 + 9;
+console.log(`150 + 9 = ${sum}`);
+
+const product = 100 * 90;
+console.log(`100 * 90 = ${product}`);
+
+let isActive = true;
+console.log(`Is the user active?: ${isActive}`);
+
+let quotient = 0;
+console.log(`The result of division is: ${quotient}.`);
+
+let favoriteRestaurants = ["Jollibee", "Mcdo", "KFC", "Dooki", "Contis"];
+console.log(favoriteRestaurants);
