c389e1dbf9ffe7ab4bf6027d967953d4f904fe60
Ayumi Kumagai <yumimori22@gmail.com>
2023-03-21 10:12:41 +0800

    Add activity Code

---
+++ s01/indexActivity.js
@@ -0,0 +1,9 @@
+console.log("Hello, Welcome to De La Salle University!")
+let person = {
+    firstName: "Ayumi",
+    lastName: "Kumagai",
+    age: 22,
+    hobbies: ["Volleyball", "Video Games", "Coding", "Listening to Music", "Reading Manga"],
+    homeAddress: {houseNumber:"13-15", street:"Branch Street", subdivision:"Filinvest", city:"Dasmarinas", province:"Cavite", zipCode:"4114"}
+}
+console.log(person)
